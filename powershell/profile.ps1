5cf10751b2af70128f21a802987858df53b263a2
Todd H. Gardner <todd@wocketware.com>
2013-10-02 10:06:42 -0500

    added chocolatey config

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -8,6 +8,8 @@ $modulePath = (join-path $powershell module)
 $env:PSModulePath = $modulePath + ";" + $env:PSModulePath
 
 $env:PHANTOMJS_BIN = "phantomjs.exe" #Expect it has been loaded on the path by chocolatey
+$env:CHROME_BIN = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+$env:FIREFOX_BIN = "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
 
 $scriptPath = (join-path $powershell script)
 Add-PathVariable $scriptPath
