a4d640e2aa02740688f358369dfcefd08e6d8ae4
Todd H. Gardner <todd@wocketware.com>
2013-10-06 15:41:44 -0500

    added npp settings

--- setup/setup-environment.ps1
+++ setup/setup-environment.ps1
@@ -3,8 +3,9 @@
 
 Import-Module 	"..\powershell\module\Pscx"
 
-New-Symlink 	"..\..\AppData\Roaming\ConEmu.xml"				"..\conemu.xml"
-New-Symlink 	"..\..\.gitconfig"								"..\.gitconfig"
+New-Symlink 	"..\..\AppData\Roaming\ConEmu.xml"		        "..\conemu.xml"
+New-Symlink 	"..\..\.gitconfig"								            "..\.gitconfig"
 
-New-Junction 	"..\..\Documents\WindowsPowerShell"				"..\powershell"
-New-Junction 	"..\..\Documents\Visual Studio 2012\Settings" 	"..\vs2012"
+New-Junction 	"..\..\Documents\WindowsPowerShell"				    "..\powershell"
+New-Junction 	"..\..\AppData\Roaming\Notepad++"             "..\notepad++"
+New-Junction 	"..\..\Documents\Visual Studio 2012\Settings" "..\vs2012"
