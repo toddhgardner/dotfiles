d97cf0c64463214a9b2e96fda2bd59840292fffe
Todd H. Gardner <todd@wocketware.com>
2013-10-06 14:39:08 -0500

    added gitconfig setup and portable conemu

--- setup/setup-environment.ps1
+++ setup/setup-environment.ps1
@@ -1,6 +1,10 @@
-
-Import-Module ..\powershell\module\Pscx
-
 # Presumes we are in HOME\dotfiles
-New-Junction ..\..\Documents\WindowsPowerShell ..\powershell
-New-Junction "..\..\Documents\Visual Studio 2012\Settings" ..\vs2012
+# Must be run as administrator
+
+Import-Module 	"..\powershell\module\Pscx"
+
+New-Symlink 	"..\..\AppData\Roaming\ConEmu.xml"				"..\conemu.xml"
+New-Symlink 	"..\..\.gitconfig"								"..\.gitconfig"
+
+New-Junction 	"..\..\Documents\WindowsPowerShell"				"..\powershell"
+New-Junction 	"..\..\Documents\Visual Studio 2012\Settings" 	"..\vs2012"
