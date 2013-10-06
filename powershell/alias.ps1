9ae1242073e5f2613299103798e9343cd625ca5c
Todd H. Gardner <todd@wocketware.com>
2013-10-06 16:54:06 -0500

    added notepad config

--- powershell/alias.ps1
+++ powershell/alias.ps1
@@ -5,4 +5,5 @@ set-alias   npp     "C:\Program Files (x86)\Notepad++\Notepad++.exe"
 set-alias   open    explorer
 set-alias   count   measure-object
 set-alias   whence  get-commandInfoEx
+set-alias   sudo    invoke-elevated
 set-alias   iis     "C:\Windows\System32\inetsrv\InetMgr.exe"
